7972860a301372fd0767266dd66c3b31dd306959
Andre <asalmeri@stud.hs-bremen.de>
2020-05-15 19:41:25 +0200

    second

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mysql = require('mysql');
+
+//Create connectoin
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password : "secret",
+    database: 'mysql'
+});
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log("Database connection established");
+})
+
+const app = express();
+
+//CREATE DB
+app.get('/createdb', (req,res) => {
+    let sql = 'CREATE DATABASE nodemysql';
+    db.query(sql, (err,result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('db created');
+    })
+})
+
+app.get('/addUser', (req, res) => {
+    let data = {}
+    let sql = 'INSERT INTO db.User SET ?';
+    let query = db.query(sql,data, (err,result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('user added');
+    });
+});
+
+app.listen('8081', () => {console.log('server started on port 8081');});
